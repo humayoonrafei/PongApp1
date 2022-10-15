e880917cf6ca71b678733e0932a36da23c434fc9
hqrafei <humayoonrafei1@gmail.com>
2022-10-15 13:30:11 -0700

    The Game has the bat and the ball works only vertically

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.HashSet;
@@ -17,13 +18,71 @@ import java.util.Set;
 
 
 public class Main extends Application {
-    Point2D size = new Point2D(1000,800);
+
+    private static final int APP_WIDTH = 800;
+    private static final int APP_HEIGHT = 600;
+    public static final int BALL_SIZE = 50;
+
+    public double getBallSpeedY() {
+        return ballSpeedY;
+    }
+
+    public void setBallSpeedY(double ballSpeedY) {
+        this.ballSpeedY = ballSpeedY;
+    }
+
+    private double ballSpeedY = 5;
+
+    public boolean isBallUp() {
+        return isBallUp;
+    }
+
+    public void setBallUp(boolean ballUp) {
+        isBallUp = ballUp;
+    }
+
+    public double getBallVelocityY() {
+        return ballVelocityY;
+    }
+
+    /**
+     * This function only calculates the y axis velocity  so we can have x
+     * velcoity indepently   is just for the ball. to make the ball go up or
+     * down at a given speed
+     */
+    public void calculateBallVelocityY() {
+     //   this.ballVelocityY = ballVelocityY;
+        if(isBallUp()){
+            this.ballVelocityY = -getBallSpeedY();
+        }
+        else {
+            this.ballVelocityY = getBallSpeedY();
+        }
+    }
+
+    private double ballVelocityY = 0 ;
+    private boolean isNewGame = true;
+
+    private boolean isBallUp = false;
+    private boolean isBallLeft = false;
+
     Set<KeyCode> KeysDown = new HashSet<>();
 
     @Override
     public void start(Stage stage) throws Exception {
         Group gRoot = new Group();
-        Scene scene = new Scene(gRoot, size.getX(), size.getY());
+
+        Rectangle ball = new Rectangle(BALL_SIZE,BALL_SIZE);
+        ball.setFill(Color.BLUE);
+        gRoot.getChildren().add(ball);
+
+
+        Rectangle bat = new Rectangle(80,20);
+        bat.setFill(Color.RED);
+        bat.setTranslateY(APP_HEIGHT-bat.getHeight());
+        gRoot.getChildren().add(bat);
+
+        Scene scene = new Scene(gRoot, APP_WIDTH, APP_HEIGHT);
 
         stage.setScene(scene);
         stage.setTitle("Pong-Game");
@@ -54,30 +113,63 @@ public class Main extends Application {
 
         // Setup of the Game //
 
+
         AnimationTimer loop = new AnimationTimer() {
 
             double old = -1; // The old time
             double elspsedTime = 0;
 
             public void handle(long nano) {
-                if (old < 0) old = nano;   // if old is negative
-                double delta = (nano - old) / 1e9;
+                calculateBallVelocityY();
+                ballFallsBottomOfScreenRestartGame(ball);
+                ball.setTranslateY(ball.getTranslateY() + getBallVelocityY()); // Moving the ball
+                ballHitsAppTopGoDown(ball);
+                // When the ball hits the bat make the ball go up again
+                if (ball.getBoundsInParent().intersects(bat.getBoundsInParent())){
+                   // setBallSpeedY(-5);
+                    setBallUp(true);
+                }
 
-                old = nano;
 
-                elspsedTime += delta;
-
-                fpsLabel.setText(String.format("%.2f %.2f", 1 / delta, elspsedTime));
-
-                //Game Loop
-            }
+            }   //Game Loop
         };
 
 
 
         loop.start();
 
+        scene.setOnMouseMoved(e->{
+            bat.setTranslateX(e.getX());
+
+        });
+
+
         stage.show();
+    }
+
+    private void ballFallsBottomOfScreenRestartGame(Rectangle ball) {
+        // ball falls to bottom of the scree restart the Game.
+        if (isNewGame){
+            isNewGame = false;
+            //TODO set ballspeed x set it to zero
+            setBallSpeedY(5);
+            ball.setTranslateX(100);
+
+            ball.setTranslateY(100);
+        }
+        if (ball.getTranslateY()>= APP_HEIGHT){
+            isNewGame = true;
+        }
+    }
+
+    private void ballHitsAppTopGoDown(Rectangle ball) { //TODO change the name to a meaningfull name
+        // When the ball is at top make it go down again
+        if (ball.getTranslateY()== 0){
+            //setBallSpeedY(5);
+            setBallUp(false);
+            ball.setFill(Color.RED);
+            ball.setTranslateY(ball.getTranslateY()+getBallVelocityY());
+        }
     }
 
     public static void main(String[] args) {
